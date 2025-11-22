037ccb3a6c63c91f2173da0af733534f03d5eb4b
joaquinmirma <joaquinmirma@gmail.com>
2025-11-22 08:50:49 +0100

    Kubernetes deploy

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY app.py .
-COPY model.pkl .
+COPY models/model.pkl .
 
 EXPOSE 5000
 
--- data/generate_iris_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.datasets import load_iris
-
-iris = load_iris()
-df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-df["target"] = iris.target
-df.to_csv("iris_dataset.csv", index=False)
-print("Archivo iris_dataset.csv creado exitosamente.")
